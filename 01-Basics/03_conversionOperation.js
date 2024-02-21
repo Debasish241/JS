6b33d5010579a73904bf859f83568fc0b6e6b8f3
Debasish Debnath <91756891+Debasish241@users.noreply.github.com>
2024-02-21 12:33:41 +0000

    conversionoperator

---
+++ 01-Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score= undefined
+let valueInNumber= Number (score);
+
+console.log(typeof valueInNumber);
+
+console.log( valueInNumber);
+
+let isLoggedIn="Debasish";
+let booloeanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booloeanIsLoggedIn);
+
+let someNumber=22;
+let stringNumber= String(someNumber);
+console.log(typeof(someNumber));
